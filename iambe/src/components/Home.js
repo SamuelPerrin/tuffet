cc9982b88fd70cd867afb8de2ead1b8ecea86b03
SamuelPerrin <samwperrin@yahoo.com>
2021-04-23 17:15:15 -0500

    add Section component to Home page

--- iambe/src/components/Home.js
+++ iambe/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Section from './styled/Section';
 import {YellowSpan, RedSpan, BlueSpan} from './styled/Spans';
 
 const initialValue = ""
@@ -20,18 +21,22 @@ const Home = () => {
 
   return (
     <div>
-      <h3>Poetry is easier than ever with Iambe!</h3>
-      <ul>
-        <li>Visualize <RedSpan>rhyme schemes</RedSpan>!</li>
-        <li>Detect over a dozen kinds of <YellowSpan>slant rhyme</YellowSpan>!</li>
-        <li>Identify <BlueSpan>meter</BlueSpan> and <BlueSpan>verse form</BlueSpan>!</li>
-      </ul>
-      <h3>Try it!</h3>
-      <form>
-        <textarea value={poem} onChange={handleChange} placeholder="Enter a poem..." />
-        <button onClick={submitRhymes} />
-        <button onClick={submitMeter} />
-      </form>
+      <Section>
+        <h3>Poetry is easier than ever with Iambe!</h3>
+        <ul>
+          <li>Visualize <RedSpan>rhyme schemes</RedSpan>!</li>
+          <li>Detect over a dozen kinds of <YellowSpan>slant rhyme</YellowSpan>!</li>
+          <li>Identify <BlueSpan>meter</BlueSpan> and <BlueSpan>verse form</BlueSpan>!</li>
+        </ul>
+      </Section>
+      <Section>
+        <h3>Try it!</h3>
+        <form>
+          <textarea value={poem} onChange={handleChange} placeholder="Enter a poem..." />
+          <button onClick={submitRhymes} />
+          <button onClick={submitMeter} />
+        </form>  
+      </Section>
     </div>
   )
 }
