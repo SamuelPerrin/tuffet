aa108a4c6f89202dc0dcdb372dd58325ac6f1128
SamuelPerrin <samwperrin@yahoo.com>
2021-04-25 16:25:29 -0500

    add constants to utils

---
+++ iambe/src/utils/phonstants.js
@@ -0,0 +1,76 @@
+// this file contains constants to be used in other utils
+export const MAKES_PLURAL_WITH_S = {'F':true, 'K':true, 'P':true, 'T':true, 'TH':true,};
+export const MAKES_PLURAL_WITH_IZ = {'S':true, 'Z':true, 'SH':true, 'ZH':true, 'CH':true, 'JH':true,};
+export const VOWELS = {
+  'AA':true,
+  'AE':true,
+  'AH':true,
+  'AO':true, 
+  'AW':true,
+  'AY':true,
+  'EH':true,
+  'ER':true,
+  'EY':true,
+  'IH':true,
+  'IY':true,
+  'OW':true,
+  'OY':true,
+  'UH':true,
+  'UW':true,
+};
+export const DETERMINERS = {
+  'a':true,
+  'an':true,
+  'the':true,
+  'my':true,
+  'thy':true,
+  'his':true,
+  'its':true,
+  'our':true,
+  'your':true,
+  'their':true,
+};
+export const PREPOSITIONS = {
+  'as':true,
+  'at':true,
+  'by':true,
+  'for':true,
+  'from':true,
+  'in':true,
+  'of':true,
+  'on':true,
+  'thro':true,
+  'through':true,
+  'to':true,
+  'with':true,
+}
+export const PER_PRON_OBJ = {
+  'me':true,
+  'thee':true,
+  'him':true,
+  'her':true,
+  'it':true,
+  'us':true,
+  'you':true,
+  'them':true,
+}
+export const PER_PRON_SUB = {
+  'i':true,
+  'thou':true,
+  'he':true,
+  'she':true,
+  'it':true,
+  'we':true,
+  'you':true,
+  'they':true,
+}
+export const VERB_TO_BE = {
+  'am':true,
+  'art':true,
+  'is':true,
+  'are':true,
+  'was':true,
+  'wast':true,
+  'were':true,
+  'wert':true,
+}
