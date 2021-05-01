c8a113176cb6d47d4b65333f30326d14818ee837
SamuelPerrin <samwperrin@yahoo.com>
2021-05-01 16:51:12 -0500

    write Line.getTokens and its tests

--- iambe/src/utils/Line.js
+++ iambe/src/utils/Line.js
@@ -1,8 +1,24 @@
-
+import * as phonstants from './phonstants';
 
 class Line {
+  // a string that represents a line of verse
   constructor(text) {
     this.text = text.trim();
+  }
+
+  getTokens() {
+    /*
+     * returns an order-preserving list of strings representing the words in the line, with punctuation removed
+     */
+    let line = this.text;
+    for (let i in phonstants.PUNCTS_TO_DELETE) {
+      line = line.replace(phonstants.PUNCTS_TO_DELETE[i],'');
+    }
+    for (let i in phonstants.PUNCTS_TO_SPACE) {
+      line = line.replace(phonstants.PUNCTS_TO_SPACE[i],' ');
+    }
+
+    return line.split(' ');
   }
 }
 
--- iambe/src/utils/Line.test.js
+++ iambe/src/utils/Line.test.js
@@ -1,7 +1,13 @@
 import Line from './Line';
 
 test('constructor trims extra spaces', () => {
-  const newLine = new Line(" testing the constructor ")
+  const newLine = new Line(" testing the constructor ");
   expect(newLine.text).toEqual("testing the constructor");
   expect(newLine.text).not.toEqual(" testing the constructor ");
 })
+
+test('getTokens splits line into list and removes punctuation', () => {
+  const newLine = new Line(`'Tis, better? to: have-loved; "and (lost).`);
+  expect(newLine.getTokens()).toEqual(["'Tis",'better','to','have','loved','and','lost']);
+})
+
