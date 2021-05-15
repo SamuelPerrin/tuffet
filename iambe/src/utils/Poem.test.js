c05da19a9a6f81458a5bcdb34c4da5a83985bf37
SamuelPerrin <samwperrin@yahoo.com>
2021-05-15 10:10:03 -0500

    adds test for Poem.getRhymes

--- iambe/src/utils/Poem.test.js
+++ iambe/src/utils/Poem.test.js
@@ -9,3 +9,8 @@ test('getStanzas splits poem into a list', () => {
   const mouse = new Poem("Me up at does\nout of the floor\nquietly Stare\na poisoned mouse\n\nstill who alive\nis asking What\nhave i done that\nYou wouldn't have");
   expect(mouse.getStanzas()).toStrictEqual(["Me up at does\nout of the floor\nquietly Stare\na poisoned mouse","still who alive\nis asking What\nhave i done that\nYou wouldn't have"]);
 })
+
+test('getRhymes returns a list of rhyme objects from each stanza', () => {
+  const alms = new Poem("As by the dead we love to sit\nBecome so wondrous dear\nAs for the lost we grapple\nThough all the rest are here\n\nIn broken mathematics\nWe estimate our prize\nVast in its fading ratio\nTo our penurious eyes");
+  expect(alms.getRhymes()).toStrictEqual([{lines:["As by the dead we love to sit","Become so wondrous dear","As for the lost we grapple","Though all the rest are here"], words:["dear","here"], rt:'full rhyme'}, {lines:["In broken mathematics","We estimate our prize","Vast in its fading ratio","To our penurious eyes"], words:["prize","eyes"], rt:'full rhyme'}])
+})
