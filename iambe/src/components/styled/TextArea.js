dad642ebd9dfa22818092ed9a80aa5868a4cb9f8
SamuelPerrin <samwperrin@yahoo.com>
2021-05-28 13:44:19 -0700

    re-styles TextArea component

--- iambe/src/components/styled/TextArea.js
+++ iambe/src/components/styled/TextArea.js
@@ -4,16 +4,13 @@ import styled from 'styled-components';
 const TextAreaWrapper = styled.textarea`
   width: ${props => props.theme.textAreaWidth};
   margin-bottom: ${props => props.theme.space};
-  textarea {
-    border-radius: ${props => props.theme.borderRadius};
-    background-color: ${props => props.theme.white};
-    color: ${props => props.theme.black};
-    font-size: ${props => props.theme.inputFontSize};
-    width: '100%';
-    height: ${props => props.theme.textAreaHeight};
-    padding: ${props => props.theme.textAreaPadding};
-    border: '12px solid ${props => props.theme.black}';
-  }
+  border-radius: ${props => props.theme.borderRadius};
+  background-color: ${props => props.theme.white};
+  color: ${props => props.theme.black};
+  font-size: ${props => props.theme.inputFontSize};
+  height: ${props => props.theme.textAreaHeight};
+  padding: ${props => props.theme.textAreaPadding};
+  border: '12px solid ${props => props.theme.black}';
 `
 
 export default function TextArea(props) {
