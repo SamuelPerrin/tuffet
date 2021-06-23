3764eddc4cc93e67c4903afde319ca92a3a1cdf4
SamuelPerrin <samwperrin@yahoo.com>
2021-06-23 12:27:42 -0500

    adds toast error to Home page

--- iambe/src/components/styled/Button.js
+++ iambe/src/components/styled/Button.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import {WHITE} from '../../constants/colors';
 
 const ButtonWrapper = styled.button`
-  color:${WHITE};
+  color:${props => props.theme.white};
   cursor: pointer;
   outline: none;
   border: none;
---
+++ iambe/src/components/styled/Toast.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ToastWrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: flex-end;
+  width:100%;  
+  position:absolute;
+
+  .container {
+    display:flex;
+    justify-content:flex-end;
+    align-items:center;
+
+    background-color: ${props => {
+      switch(props.variant) {
+        case 'danger':
+          return props.theme.dangerColor;
+        case 'success':
+          return props.theme.green;
+          default:
+            return props.theme.blue;
+          }
+      }};
+    color: ${props => props.theme.white};
+    max-width: 18rem;
+    margin: ${props => props.theme.space};
+    padding: 0 1rem 0 0;
+
+    position: relative;
+    bottom: 2rem;
+    width: 100%;
+  }
+
+  div.icon {
+    font-size:1.5rem;
+    width:2rem;
+    height:2rem;
+    border-radius:50%;
+    border: 1px solid ${props => props.theme.white};
+    margin: 0.5rem;
+    padding: 0.25rem;
+    text-align: center;
+    line-height: 1.25rem;
+
+    &:hover {
+      background-color: ${props => props.theme.white};
+      color: ${props => {
+      switch(props.variant) {
+        case 'danger':
+          return props.theme.dangerColor;
+        case 'success':
+          return props.theme.green;
+        default:
+          return props.theme.blue;
+        }
+      }};
+      border: 1px solid ${props => {
+      switch(props.variant) {
+        case 'danger':
+          return props.theme.dangerColor;
+        case 'success':
+          return props.theme.green;
+        default:
+          return props.theme.blue;
+        }
+      }};
+    }
+  }
+`
+
+const Toast = props => {
+  const {children, variant, onClick} = props;
+
+  return (
+    <ToastWrapper variant={variant} onClick={onClick}>
+      <div className='container'>
+        {variant === 'danger' && <div className='icon'>x</div>}
+        {children}
+      </div>
+    </ToastWrapper>
+  )
+}
+
+export default Toast;
