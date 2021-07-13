978e446666c77acdbb0b1da2c390a08898dd682e
SamuelPerrin <samwperrin@yahoo.com>
2021-07-13 15:20:48 -0500

    adds java project

---
+++ be/src/main/java/swper/iambedb/IambeDbApplication.java
@@ -0,0 +1,13 @@
+package swper.iambedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IambeDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IambeDbApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/swper/iambedb/IambeDbApplicationTests.java
@@ -0,0 +1,13 @@
+package swper.iambedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IambeDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
